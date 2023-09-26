542270bb6fddc68ada063332ddb131d6e9373529
ErikLaN68 <eriklaneave@gmail.com>
2023-09-25 22:38:49 -0600

    Works for declaration

--- wordCount.py
+++ wordCount.py
@@ -13,27 +13,39 @@ inputFile = open(argv[1], "r")
 inputString = inputFile.readlines()
 print("Number of lines in file " + str(len(inputString)))
 
-wordDict = dict()
-for line in inputString:
-    tokens = line.split(" ")
-    for word in tokens:
-        word = word.lower().strip()
-        if not word.isalpha():
-            word = word.replace(".","").replace(",","").replace(";","").replace(":","")
-            if word in wordDict and len(word) != 0:
-                count = wordDict.get(word)
-                count = count + 1
-                wordDict.update({word: count})
-            else:
-                if len(word) != 0:
-                    wordDict.update({word: 1})
-        elif word in wordDict and len(word) != 0:
+def addToDict(word):
+    if not word.isalpha():
+        word = word.replace(".","").replace(",","").replace(";","").replace(":","")
+        if word in wordDict and len(word) != 0:
             count = wordDict.get(word)
             count = count + 1
             wordDict.update({word: count})
         else:
             if len(word) != 0:
                 wordDict.update({word: 1})
+    elif word in wordDict and len(word) != 0:
+        count = wordDict.get(word)
+        count = count + 1
+        wordDict.update({word: count})
+    else:
+        if len(word) != 0:
+            wordDict.update({word: 1})
+
+wordDict = dict()
+for line in inputString:
+    tokens = line.split(" ")
+    for word in tokens:
+        word = word.lower().strip()
+        if "-" in word:
+            tempWord = word.split("-")
+            for w in tempWord:
+                addToDict(w)
+        elif "'" in word:
+            tempWord = word.split("'")
+            for w in tempWord:
+                addToDict(w)
+        else:
+            addToDict(word)
             
 print("Words in Dict " + str(len(wordDict)))
 keyList = list(wordDict.keys())
@@ -44,4 +56,4 @@ outputFile = open(argv[2], "w")
 for key in keyList:
     outputFile.write(key + " " + str(wordDict.get(key)) + "\n")
     
-print(wordDict.get(""))
+print(wordDict.get("s"))
